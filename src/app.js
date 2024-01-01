baa3ef974255afc73be7644963814fcd019bfbdb
youngkhaf <youngkhaf@gmail.com>
2024-01-01 12:58:36 +0000

    checkpoint

--- src/app.js
+++ src/app.js
@@ -10,19 +10,23 @@ const { socketClientStore } = require('./socketClientStore')
 const { globalApiKey } = require('./config')
 // Initialize Express app
 const corsOptions = {
-  origin: '*',
+  origin: true,
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-  credentials: true,
-  optionsSuccessStatus: 204
+  optionsSuccessStatus: 200
 }
-app.disable('x-powered-by')
+
 app.use(cors(corsOptions))
 app.options('*', cors())
+
+app.disable('x-powered-by')
 app.use(bodyParser.json({ limit: maxAttachmentSize + 1000000 }))
 app.use(bodyParser.urlencoded({ limit: maxAttachmentSize + 1000000, extended: true }))
 app.use('/', routes)
 const server = require('http').createServer(app)
-const socketServer = new Server(server)
+const socketServer = new Server(server, {
+  cors: corsOptions
+})
+
 socketServer.on('connection', (socket) => {
   const authKeys = socket.request.headers
   console.log({ authKeys })
