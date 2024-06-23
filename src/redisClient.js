4a7a0627156e6b1710a08f2c50aac1a222765525
youngkhaf <youngkhaf@gmail.com>
2024-06-23 22:30:41 +0000

    retry strategy implemented

--- src/redisClient.js
+++ src/redisClient.js
@@ -2,6 +2,22 @@ const { createClient } = require('redis')
 let redisClient = null
 try {
   redisClient = createClient({
+    retry_strategy: function (options) {
+      if (options.error && options.error.code === 'ECONNREFUSED') {
+        // If redis refuses the connection or is not able to connect
+        return new Error('The server refused the connection')
+      }
+      if (options.total_retry_time > 1000 * 60 * 60) {
+        // End reconnection after the specified time limit
+        return new Error('Retry time exhausted')
+      }
+      if (options.attempt > 10) {
+        // End reconnecting with built in error
+        return undefined
+      }
+      // reconnect after
+      return Math.min(options.attempt * 100, 3000)
+    },
     url: 'redis://api.sentek.app:6379',
     password: 'gRuL£TED65A5>TjQ15x'
   })
@@ -19,6 +35,11 @@ try {
   })
 } catch (err) {
   console.error('Redis error:', err)
+  // return a "moked" redis instance
+  redisClient = {
+    set: () => null,
+    get: () => null
+  }
 }
 
 module.exports = redisClient
